0310333f5ab878d1f7f6eb4fb65df3edf03e5f31
Sara <srancati0@gmail.com>
2019-12-16 17:37:26 +0000

    Refactored validation methods for deposit/withdrawal to check that amount can only be entered as a number

--- lib/account.rb
+++ lib/account.rb
@@ -9,18 +9,31 @@ class Account
     end
 
     def deposit(date, amount)
-        @balance += amount
-        this_transaction = [[date, " || || ", sprintf("%.2f", amount), " || ", sprintf("%.2f", @balance)].join()]
-        @transactions.insert(1, this_transaction)
-        return this_transaction 
+        if validate(date, amount) == false
+            fail 'Please enter a valid input for date and/or amount'
+        else
+            @balance += amount
+            this_transaction = [[date, " || || ", sprintf("%.2f", amount), " || ", sprintf("%.2f", @balance)].join()]
+            @transactions.insert(1, this_transaction)
+            return this_transaction 
+        end
     end
 
     def withdraw(date, amount)
-        @balance -= amount
-        this_transaction = [[date, " || ", sprintf("%.2f", amount), " || || ", sprintf("%.2f", @balance)].join()]
-        @transactions.insert(1, this_transaction)
-        return this_transaction 
+        if validate(date, amount) == false
+            fail 'Please enter a valid input for date and/or amount'
+        else
+            @balance -= amount
+            this_transaction = [[date, " || ", sprintf("%.2f", amount), " || || ", sprintf("%.2f", @balance)].join()]
+            @transactions.insert(1, this_transaction)
+            return this_transaction 
+        end
     end
+
+    def print_statement
+        @output.puts @transactions
+        return @transactions.join("\n")
+    end 
 
     def check_date(date)
         if Date.today >= DateTime.parse(date)
@@ -30,9 +43,14 @@ class Account
         end
     end
 
-    def print_statement
-        @output.puts @transactions
-        return @transactions.join("\n")
-    end 
+    private
+
+    def validate(date, amount)
+        if check_date(date) == false || amount.is_a?(Numeric) == false
+            return false
+        else
+            return true
+        end
+    end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -7,6 +7,15 @@ describe Account do
         it 'saves the date and amount in the right order as string' do
             expect(subject.deposit("14/01/2012", 500.00)).to eq(["14/01/2012 || || 500.00 || 500.00"])
         end
+
+        it 'raises an error if the input for the amount is not a number' do
+            expect { subject.deposit("14/01/2012", "five") }.to raise_error('Please enter a valid input for date and/or amount')
+        end
+
+        it 'raises an error if the transaction date is not in the past' do
+            expect { subject.deposit("14/01/2022", 300.00) }.to raise_error('Please enter a valid input for date and/or amount')
+        end
+
     end
 
     describe '#withdraw' do
@@ -14,6 +23,15 @@ describe Account do
             subject.deposit("14/01/2012", 1000.00)
             expect(subject.withdraw("14/01/2012", 500.00)).to eq(["14/01/2012 || 500.00 || || 500.00"])
         end
+
+        it 'raises an error if the input for the amount is not a number' do
+            expect { subject.withdraw("14/01/2012", "five") }.to raise_error('Please enter a valid input for date and/or amount')
+        end
+
+        it 'raises an error if the transaction date is not in the past' do
+            expect { subject.withdraw("14/01/2022", 300.00) }.to raise_error('Please enter a valid input for date and/or amount')
+        end
+
     end
 
     describe '#date_check' do
