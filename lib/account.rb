60e77721d853d59e47cccccf57563bfacc57b65a
Sara <srancati0@gmail.com>
2019-12-18 16:12:01 +0000

    Refactoring: extracted Printer class

--- lib/account.rb
+++ lib/account.rb
@@ -1,42 +1,37 @@
 # frozen_string_literal: true
 
 require 'date'
+require_relative 'printer'
 
 class Account
 
   def initialize(balance = 0)
     @balance = balance
-    @transactions = ['date || credit || debit || balance']
+    @transactions = []
   end
 
   def deposit(amount, date = Time.now.strftime("%d/%m/%Y"))
     validate(date, amount) 
     @balance += amount
-    deposit = format_string(date, amount, @balance).insert(3, "||")
-    add_to_list(@transactions, deposit)
+    @transactions.unshift([date, "||", amount, "|| ||", @balance])
   end
 
   def withdraw(amount, date = Time.now.strftime("%d/%m/%Y"))
     validate(date, amount)
     raise "The amount exceeds the available balance by #{format('%.2f', (amount - @balance))}" if (amount - @balance).positive?
     @balance -= amount
-    withdrawal = format_string(date, amount, @balance).insert(1, "||")
-    add_to_list(@transactions, withdrawal)
+    @transactions.unshift([date, "|| ||", amount, "||", @balance])
   end
 
   def wrong_date?(date)
     Date.today < DateTime.parse(date)
   end
 
-  private
-
-  def add_to_list(list, transaction)
-    list.insert(1, transaction.join(" "))
-  end 
-  
-  def format_string(date, amount, balance)
-    [date.gsub(/-/, '/'), "||", format('%.2f', amount), "||", format('%.2f', balance)]
+  def print_statement(printer = Printer.new)
+    printer.print_formatted_list(@transactions)
   end
+
+  private
 
   def validate(date, amount)
     if wrong_date?(date) || amount.is_a?(Numeric) == false || amount <= 0
--- lib/printer.rb
+++ lib/printer.rb
@@ -1,10 +1,28 @@
-require 'account'
-
 class Printer
+    def initialize
+        @statement_columns = ['date || credit || debit || balance']
+    end
   
-    def print(transactions)
-        # formatting
-        p statement
+    def format_numbers(list)
+        list.map do |element|
+            element.map do |item| 
+                if item.is_a?(Numeric) 
+                    item = format('%.2f', item)
+                elsif item.is_a?(String)
+                    item = item.gsub(/-/, '/')
+                else
+                    item
+                end
+            end
+        end
+    end
+
+    def print_formatted_list(list)
+        formatted_list = []
+        format_numbers(list).map do |string|
+            formatted_list.push(string.join(" "))
+        end 
+        puts formatted_list.unshift(@statement_columns)
     end
 
 end
